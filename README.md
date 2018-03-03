7170f123af9fbaf4714ad966460642ca37eb845a
Ankur <ankur.pes@gmail.com>
2018-03-03 18:24:10 +0530

    modified some files

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# CNS
-This repository contains CNS assignments
+# Xmas Scan
+This repository contains code for detecting Xmas Scans.
+
+# Prerequisites
+* dpkt==1.9.1
+
+# Installing Prerequisites
+pip install dpkt
+
+# Introduction
+An attacker uses a TCP Xmas Scan to determine which ports are closed on the target machine. 
+This scan type is accomplished by sending TCP segments with the FIN, PSH and URG flags set in the packet header. These segments are illegal according to RFC 793. The expected behavior is that any illegal TCP segment sent to an open port is discarded, whereas illegal segments sent to closed ports are handled with a RST in response.
+
+This behavior allows an attacker to scan for closed ports by sending rule-breaking packets and detect closed ports via RST packets. The major advantage of this scan type is its ability to scan through stateless firewall or ACL filters. Such filters are configured to block access to ports usually by preventing SYN packets, thus stopping any attempt to 'build' a connection. XMAS packets tend to pass through such devices undetected. 
+
+Additionally, because open ports are inferred via no responses being generated, Xmas scan cannot distinguish an open port from a filtered port.
+
+# Executing the code
+python xmas.py xmas.pcap
--- xmas.py
+++ xmas.py
@@ -1,17 +1,27 @@
+# Xmas scan
+from collections import defaultdict
+import sys
 import dpkt
 import socket
-from collections import defaultdict
 
-f = open('xmas.pcap')
+# Opening the pcap file passed as a command line argument
+f = open(sys.argv[1])
+
+# Passing the file to the Reader class for reading records from the file
 pcap = dpkt.pcap.Reader(f)
-ip_count = defaultdict(int)
 
+# Dictionaries to store the number of packets sent and received by different hosts
+src_ip_count = defaultdict(int)
+dst_ip_count = defaultdict(int)
+
+# This function converts an inet object to a string
 def inet_to_str(inet):
     try:
         return socket.inet_ntop(socket.AF_INET, inet)
     except ValueError:
         return socket.inet_ntop(socket.AF_INET6, inet)
 
+# Parsing the pcap file
 for ts, buf in pcap:
     eth = dpkt.ethernet.Ethernet(buf)
 
@@ -20,21 +30,19 @@ for ts, buf in pcap:
 
         if type(ip.data) == dpkt.tcp.TCP:
             tcp = ip.data
+
             if tcp.flags==41:
-                ip_count[inet_to_str(ip.src)]+=1
-                #print inet_to_str(ip.dst)  
+                src_ip_count[inet_to_str(ip.src)]+=1
+                
+            elif tcp.flags==20:
+                dst_ip_count[inet_to_str(ip.dst)]+=1
 
-    """tcp = ip.data
-    if type(tcp) == dpkt.tcp.TCP:
-        if tcp.dport == 80 and len(tcp.data) > 0:
-            http = dpkt.http.Request(tcp.data)
-            print http.uri"""
-
-    """if tcp.dport == 80 and len(tcp.data) > 0:
-        http = dpkt.http.Request(tcp.data)
-        print http.uri"""
-
-for ip in ip_count.keys():
-    print(ip)
+# printing the ip address of the attacker (person who is possibly doing a Xmas scan)
+for ip in src_ip_count.keys():
+    count1 = src_ip_count[ip]
+    count2 = dst_ip_count[ip]
+    count3 = count1/3
+    if count1-count2<count3:
+        print "Ip address of the attacker is",ip
 
 f.close()
